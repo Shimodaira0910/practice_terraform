f5c79a84471adda02f7f9423a8193b0ba309044a
下平大輝 <shimodairadaiki@shimodairadaiakiranoMacBook-Air.local>
2023-09-26 12:04:55 +0900

    add parameter_store config

--- appserver.tf
+++ appserver.tf
@@ -11,6 +11,40 @@ resource "aws_key_pair" "keypair" {
     Env     = var.enviroment
   }
 }
+
+# -----------------------
+# SSM Parameter Store
+# -----------------------
+resource "aws_ssm_parameter" "host" {
+  name  = "/${var.project}/${var.enviroment}/app/MYSQL_HOST"
+  type  = "String"
+  value = aws_db_instance.mysql_standalone.address
+}
+
+resource "aws_ssm_parameter" "port" {
+  name  = "/${var.project}/${var.enviroment}/app/MYSQL_PORT"
+  type  = "String"
+  value = aws_db_instance.mysql_standalone.port
+}
+
+resource "aws_ssm_parameter" "db_name" {
+  name  = "/${var.project}/${var.enviroment}/app/MYSQL_DATABASE"
+  type  = "String"
+  value = aws_db_instance.mysql_standalone.db_name
+}
+
+resource "aws_ssm_parameter" "username" {
+  name  = "/${var.project}/${var.enviroment}/app/MYSQL_USERNAME"
+  type  = "SecureString"
+  value = aws_db_instance.mysql_standalone.username
+}
+
+resource "aws_ssm_parameter" "password" {
+  name  = "/${var.project}/${var.enviroment}/app/MYSQL_PASSWORD"
+  type  = "SecureString"
+  value = aws_db_instance.mysql_standalone.password
+}
+
 # -----------------------
 # EC2 Instance
 # -----------------------
@@ -19,7 +53,7 @@ resource "aws_instance" "app_server" {
   instance_type               = "t2.micro"
   subnet_id                   = aws_subnet.public_subnet_1a.id
   associate_public_ip_address = true
-  iam_instance_profile = aws_iam_instance_profile.app_ec2_profile.name
+  iam_instance_profile        = aws_iam_instance_profile.app_ec2_profile.name
 
   vpc_security_group_ids = [
     aws_security_group.app-sg.id,
